3be9a5d71e481bbf7c7a9cb9c6621524c4b3f193
Jay Yin <mumingv@163.com>
2016-04-12 22:01:05 +0800

    add dir function

---
+++ function/stdio.h/puts.c
@@ -0,0 +1,19 @@
+/***********************************************************
+ *     FileName: puts.c
+ *         Desc: 送一字符串到流stdout中
+ *       Author: Jie Yin
+ *        Email: mumingv@163.com
+ *     HomePage: https://github.com/mumingv
+ *      Version: 0.0.1
+ *   LastChange: 2016-04-12 21:56:16
+ *      History:
+ ***********************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char string[] = "Just for c learning.\n";
+    puts(string);
+    return 0;
+}
+
